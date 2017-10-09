b7bd18e78a646c0fe0395936636887521e36becb
Juan David Cruz Serrano <juandavidcruzs@users.noreply.github.com>
2017-10-09 02:24:40 -0500

    Hello World

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+
+/* Begin Functions */
+
+//Get Nearby Places
+exports.getNearbyPlaces = functions.https.onRequest((request, response) => {
+  response.status(200).send("Places");
+});
+/* End Functions */
